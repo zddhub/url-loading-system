1985435d08eb08a9abb2e09940665534661d37ec
zdd <zddhub@gmail.com>
2022-05-07 22:02:01 +0800

    Create project

---
+++ UrlLoadingSystem/UrlLoadingSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrlLoadingSystemApp.swift
+//  UrlLoadingSystem
+//
+//  Created by Dongdong Zhang on 2022/5/7.
+//
+
+import SwiftUI
+
+@main
+struct UrlLoadingSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
